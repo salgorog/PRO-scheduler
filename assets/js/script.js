73dd63cf85c3844f9004160c31dd966889a409f0
Aztec <salgorog@hotmail.com>
2020-08-30 22:16:40 -0600

    successfully getItem from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,14 @@ $(".saveBtn").on("click", function () {
     
     localStorage.setItem(time, value);
 });
+
+
+$("#9am .task-input").val(localStorage.getItem("9am"));
+$("#10am .task-input").val(localStorage.getItem("10am"));
+$("#11am .task-input").val(localStorage.getItem("11am"));
+$("#12pm .task-input").val(localStorage.getItem("12pm"));
+$("#1pm .task-input").val(localStorage.getItem("1pm"));
+$("#2pm .task-input").val(localStorage.getItem("2pm"));
+$("#3pm .task-input").val(localStorage.getItem("3pm"));
+$("#4pm .task-input").val(localStorage.getItem("4pm"));
+$("#5pm .task-input").val(localStorage.getItem("5pm"));
