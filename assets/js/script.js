cc19c99e5c51966a8977da9328988c1b32deb928
Aztec <salgorog@hotmail.com>
2020-08-30 22:05:14 -0600

    added save to localStorage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-$('#currentTime h6').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+// Display current day at the top of the calendar
+$('#currentTime').text(moment().format('MMMM DD, YYYY'));
+
+
+
+//  save button was clicked
+$(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    var value = $(this).siblings(".task-input").val();
+    
+    localStorage.setItem(time, value);
+});
